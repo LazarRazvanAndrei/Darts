1d73f454d5302a0bdfd0c868bca8d1f10e6619d0
= <=>
2019-01-27 11:42:20 +0200

    First commit

---
+++ src/test/java/DartsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class DartsTest {
+
+    private Darts dartBoard;
+
+    @Before
+    public void setUp() {
+        dartBoard = new Darts();
+    }
+}
